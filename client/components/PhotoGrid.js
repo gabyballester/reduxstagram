7bf2674976fa2fadc7282b1f2e28fd46a67ff1f6
gabyballester <gaby.ballester@gmail.com>
2020-05-09 19:36:00 +0200

    Creado PhotoGrid Component

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from "react"; // imr snippet
+
+// cc snippet
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+                Soy el PhotoGrid
+            </div>
+        );
+    },
+});
+
+export default PhotoGrid;
