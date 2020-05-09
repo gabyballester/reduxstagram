f525c3db38c1b3e1b1dc448ef7df0530cb000297
gabyballester <gaby.ballester@gmail.com>
2020-05-09 18:53:14 +0200

    Configurado componente padre reduxstagram.js

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react'; // imr snippet
+
+import {render} from 'react-dom' //importamos render
+
+// importamos css
+import css from './styles/style.styl'
+
+// Creamos el render de la app y lo inyectamos en root (index.html)
+render(<p>Hola</p>, document.getElementById('root'));
