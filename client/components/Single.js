54a1e367acdf69b92792d06b29852fea1322930a
gabyballester <gaby.ballester@gmail.com>
2020-05-09 19:32:34 +0200

    Creado Single Component

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from "react"; // imr snippet
+
+// cc snippet
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+                Soy el Single
+            </div>
+        );
+    },
+});
+
+export default Single;
