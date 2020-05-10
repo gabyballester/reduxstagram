ed65d7d1e1a0b8fdf4e8278477108a6a0dd3ca40
gabyballester <gaby.ballester@gmail.com>
2020-05-10 18:12:33 +0200

    Creado el Comments.js component e inyectado en el Single component en que se muestra una publicación

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from "react"; // imr snippet
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                Soy un comentario
+            </div>
+        );
+    },
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,19 @@
 import React from "react"; // imr snippet
+import Photo from './Photo';
+import Comments from './Comments';
 
-// cc snippet
 const Single = React.createClass({
     render() {
+        // indice de post que coincida con el param enviado
+        const i = this.props.posts.findIndex(
+            (post) => post.code === this.props.params.postId);
+        // recuperamos el post que corresponda a ese i
+        const post = this.props.posts[i];
         return (
             <div className="single-photo">
-                Soy el Single
+                {/* pasamos i post y el resto de props con spread */}
+                <Photo i={i} post={post} {...this.props} />
+                <Comments />
             </div>
         );
     },
